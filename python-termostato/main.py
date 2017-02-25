a85ba13f01da94fb2bef1918496eb1e937a53ae9
Javier García FIllol <javier.garcia.fillol@gmail.com>
2017-02-25 13:21:23 +0100

    Added timer for every mail

--- python-termostato/main.py
+++ python-termostato/main.py
@@ -7,6 +7,8 @@ import time
 import alarm
 import os
 
+timer = 10
+
 def main():
 
   maxtemp = 25
@@ -20,22 +22,29 @@ def main():
     thread.start_new_thread(addInfo, (humidity, temperature) )
 
     if humidity > maxhumidity:
-	print("Humedad mas de {0:0.1f}%".format(maxhumidity))
-	alarmTemp = 1
+	  print("Humedad mas de {0:0.1f}%".format(maxhumidity))
+	  alarmTemp = 1
 
 
     if temperature > maxtemp:
-	print("Temperatura mas de {0:0.1f} C".format(maxtemp))
-	alarmTemp = 1
+	  print("Temperatura mas de {0:0.1f} C".format(maxtemp))
+	  alarmTemp = 1
 
     if alarmTemp == 1:
-	alarm.start()
+	  alarm.start()
+	  timer = timer +1
+	  if timer > 10:
+	    thread.start_new_thread(sendemail, () )
+	    timer = 0
 
     time.sleep(10)
 
 
 def addInfo(humidity, temperature):
    os.system("/usr/bin/curl -k -s http://127.0.0.1/insert/temp/{0:0.1f}/hum/{1:0.1f} > /dev/null".format(temperature, humidity))
+
+def sendemail():
+   os.system("/usr/bin/curl -k -s http://127.0.0.1/sendemail > /dev/null")
 
 #thread.start_new_thread(alarm.start, () )
 
